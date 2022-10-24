ee87b66ff8e0e0bc4ddf5b139b89fadc24ff10a3
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-10-24 17:25:32 +0700

    Binary & Linear Algorithm

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -56,7 +56,100 @@ namespace SearchAlgorithm
 
                 //obtain the index of the middle elements
                 int mid = (upperbound - lowerbound) / 2;
+                int ctr = 1;
+
+                //loop to search for the element in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound)) ;
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+                    mid = (upperbound - lowerbound) / 2;
+                    ctr++;
+                }
+                if (item == arr[mid])
+                    Console.WriteLine("\n" + item.ToString() + "Found at position" + (mid + 1).ToString());
+                else
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                Console.WriteLine("\n Number of Comparasion : " + ctr);
+
+                Console.Write("\nContionue search (y/n):");
+                ch = char.Parse(Console.ReadLine());
             }
+            while ((ch == 'y') || (ch == 'Y'));
+        }
+        public void LinearSearch()
+        {
+            char ch;
+            //search for number of comparison
+            int ctr;
+            do
+            {
+                //Accept the number to be searched
+                Console.Write("\n Enter the element you want to search:");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found st position" + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of comparison: " + ctr);
+                Console.Write("\nContinue search (y/n):");
+                ch = Char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
+        }
+        static void Main(string[] args)
+        {
+            Program myList = new Program();
+            int pilihanmenu;
+            do
+            {
+                Console.WriteLine("Menu Option");
+                Console.WriteLine("===========");
+                Console.WriteLine("1.Linear Search");
+                Console.WriteLine("2. BinarySearch");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter your choice (1,2,3) :");
+                pilihanmenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (pilihanmenu)
+                {
+                    case 1:
+                        Console.WriteLine("");
+                        Console.WriteLine("...............");
+                        Console.WriteLine("Linear Search");
+                        Console.WriteLine("...............");
+                        myList.input();
+                        myList.LinearSearch();
+                        break;
+                    case 2:
+                        Console.WriteLine("");
+                        Console.WriteLine("...............");
+                        Console.WriteLine("Binary Search");
+                        Console.WriteLine("...............");
+                        myList.input();
+                        myList.BinarySearch();
+                        break;
+                    case 3:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+                //to exit from the console
+                Console.WriteLine("\n\nPress return to exit.");
+                Console.ReadLine();
+            } while (pilihanmenu != 3);
         }
     }
 }
